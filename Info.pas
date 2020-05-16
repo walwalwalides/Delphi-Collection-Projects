6ad06d1fa644a2b5345978ad0c38ac1a2b1a3e08
walwalwalides <walwalwalides@gmail.com>
2020-05-16 22:16:15 +0200

    Update Info.pas

---
+++ Info.pas
@@ -0,0 +1,13 @@
+{ ============================================
+  Software Name : Delphi-Collection-Projects
+  ============================================ }
+{ ******************************************** }
+{ Written By WalWalWalides }
+{ CopyRight © 2020 }
+{ Email : WalWalWalides@gmail.com }
+{ GitHub :https://github.com/walwalwalides }
+{ ******************************************** }
+unit Info;
+interface
+implementation
+end.
